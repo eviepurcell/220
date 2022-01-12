4c3ef479e752bec4a7b3e30894f7c86dca16b1d2
pabaier <baierpa@g.cofc.edu>
2022-01-11 22:05:11 -0500

    use letter grades

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,35 +1,47 @@
 """
-Name: <your name goes here – first and last>
+Name: <Evie Purcell>
 <ProgramName>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: In this exercise we used functions to do simple math like produce percentages, showing area, and convert
+units of measure.
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 def calc_volume():
-    pass
-
+    length = eval(input("Enter the Length: "))
+    width = eval(input("Enter the Width: "))
+    height = eval(input("Enter the Height: "))
+    area = length * width * height
+    print("Volume =", area)
 
 def shooting_percentage():
-    pass
-
+    totalshot = eval(input("Enter the player's total shots: "))
+    shots = eval(input("Enter how many shots the player made: "))
+    total = (shots / totalshot) * 100
+    print(("Shooting Percentage: ", total) + '%')
 
 def coffee():
-    pass
-
+    pound = eval(input("How many pounds of coffee would you like?"))
+    total = (10.50 * pound) + (.86 * pound) + 1.50
+    print("You're total is:", total)
 
 def kilometers_to_miles():
-    pass
-
+    kilometers = eval(input("How many kilometers did you travel? "))
+    milestraveled = kilometers / 1.61
+    print("That's", milestraveled, "miles")
 
 if __name__ == '__main__':
-    pass
+
+
+
+
