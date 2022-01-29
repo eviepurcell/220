e3b0bd67e0ee48795c9f1df5b4d3e625f0f03a2f
eviepurcell <97616929+eviepurcell@users.noreply.github.com>
2022-01-29 00:11:01 -0500

    Update hw2.py

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,5 +1,5 @@
 """
-Name: <your name goes here – first and last>
+Name: <evie purcell>
 <ProgramName>.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
@@ -7,30 +7,50 @@ Problem: <Brief, one or two sentence description of the problem that this progra
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 import math
 
-
 def sum_of_threes():
-    pass
-
+    upperbound = int(input("what is the upper bound? "))
+    for i in range(1, upperbound+1):
+        print(upperbound*i, end="")
 
 def multiplication_table():
-    pass
+    for i in range(1,11):
+        for j in range(1,11):
+          print(i * j, end='\t')
+        print('')
 
 
 def triangle_area():
-    pass
+    aside = eval(input("What is the length of A side?:"))
+    bside = eval(input("What is the length of B side?:"))
+    cside = eval(input("What is the length of C side?:"))
+    side = (aside + bside + cside) / 2
+    area = (math.sqrt(side(side - aside)(side - bside)(side - cside)))
+    print("The area is:", area)
+    triangle_area()
 
 
 def sum_squares():
-    pass
+    my_sum = 0
+    for i in range(3,5):
+        my_sum = my_sum + (i * i)
+    print(sum)
 
 
 def power():
-    pass
+    base = eval(input("Enter the base number: "))
+    exponent = eval(input("Enter the base exponent: "))
+    my_sum = 1
+    for i in range(exponent):
+        my_sum = my_sum * base
+    print("my_sum", my_sum)
 
+sum_of_threes()
+multiplication_table()
+triangle_area()
+sum_squares()
+power()
 
 if __name__ == '__main__':
-    pass
